897e1d8c50768f6c1125db17ff595a423de55f73
comdyax <cody24@tuta.io>
2023-11-09 13:35:39 +0100

    uibuild

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-//Loads static content from dist directory
+//Loads static content from dist directory with production build
 app.use(express.static('dist'))
 
 morgan.token('input', (req, res) => {
